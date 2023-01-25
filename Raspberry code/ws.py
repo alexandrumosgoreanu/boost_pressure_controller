f81c369672b0978c96975956363d44fb8fb8d966
alexandrumosgoreanu <amosgoreanu@gmail.com>
2023-01-25 18:43:18 +0200

    Added working websockets connection

---
+++ Raspberry code/ws.py
@@ -0,0 +1,100 @@
+from machine import Pin, ADC
+from gpio_lcd import GpioLcd
+import time
+import wifi
+import socket
+import json
+
+from ws_connection import ClientClosedError
+from ws_server import WebSocketServer, WebSocketClient
+
+# connecting to Wifi
+try:
+    ip = wifi.connect() 
+except KeyboardInterrupt:
+    print("Couldn't connect to wifi")
+    machine.reset()
+    
+# Display Pin config
+lcd = GpioLcd(rs_pin = Pin(16),
+              enable_pin = Pin(17),
+              d4_pin = Pin(18),
+              d5_pin = Pin(19),
+              d6_pin = Pin(20),
+              d7_pin = Pin(21),
+              num_lines = 2, num_columns = 16)
+
+# ADC potentiometer on GPIO 28
+pot = ADC(27)
+
+# MAP sensor voltage
+map = ADC(26)
+
+
+
+# reading pin analog value on 16 bits
+def read_pin(pin):
+    return pin.read_u16() - 320
+    
+# converting 16 bits ADC value to voltage
+def convert_to_voltage(value):
+    return value * 3.3 / 65215
+
+# converting MAP voltage to actual measured pressure
+def map_scaling(value):
+    return (value - 0.030434) / 1.8478
+
+def readSensorData(ref, map):
+    refv = str(round(read_pin(pot) / 65215 * 1.5, 2))
+    mapv = str(round(map_scaling(convert_to_voltage(read_pin(map))), 2))
+    return refv, mapv
+
+def printToDisplay(lcd, refv, mapv):
+    lcd.putstr('Ref:' + refv + ' bar')
+    lcd.move_to(0,1)
+    lcd.putstr('MAP:'+ mapv + ' bar')
+
+
+class Senzor(WebSocketClient):
+    value = 0
+    def __init__(self, conn):
+        super().__init__(conn)
+
+    def process(self, refv, mapv):
+        try:
+            tmp = time.localtime()
+           # tmp = str(tmp[0]) + '-' + str(tmp[1]) + '-' +  str(tmp[2]) + ":" + str(tmp[3]) + '-' + str(tmp[4]) + '-' +  str(tmp[5])
+            tmp = str(tmp[3]) + ':' + str(tmp[4]) + ':' +  str(tmp[5])
+            resp = {
+                "date":tmp,
+                "targetBoost":refv,
+                "actualBoost":mapv
+                }
+            self.connection.write(json.dumps(resp))
+            
+        except ClientClosedError:
+            self.connection.close()
+
+
+
+class AppServer(WebSocketServer):
+    def __init__(self):
+        super().__init__("percentage.html", 10)
+
+    def _make_client(self, conn):
+        return Senzor(conn)
+        
+
+
+server = AppServer()
+server.start(3000)
+try:
+    while True:
+        refv, mapv = readSensorData(pot,map)
+        printToDisplay(lcd, refv, mapv)
+        server.process_all(refv,mapv)
+        lcd.clear()
+except KeyboardInterrupt:
+    pass
+server.stop()
+
